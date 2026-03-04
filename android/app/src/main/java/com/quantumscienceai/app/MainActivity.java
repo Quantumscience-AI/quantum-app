0e48e28469ac7914bef9287c64347d3ab90876f3
Jackson Wot <jacksonshelby070@gmail.com>
2026-03-04 10:10:47 +0000

    Fix Android font scaling and viewport

--- android/app/src/main/java/com/quantumscienceai/app/MainActivity.java
+++ android/app/src/main/java/com/quantumscienceai/app/MainActivity.java
@@ -2,4 +2,13 @@ package com.quantumscienceai.app;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onStart() {
+    super.onStart();
+    // Disable Android font scaling
+    android.content.res.Configuration config = getResources().getConfiguration();
+    config.fontScale = 1.0f;
+    createConfigurationContext(config);
+  }
+}
